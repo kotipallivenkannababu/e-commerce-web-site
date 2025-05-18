c83bdcde1b10fee5f56be7a9b233fdafc0892772
kotipallivenkannababu <kotipallivenkannababu8@gmail.com>
2025-05-18 08:10:22 +0530

    maps

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -12,7 +12,7 @@ function ContactUs() {
     const dateTimeInput = document.createElement('input');
     dateTimeInput.type = 'hidden';
     dateTimeInput.name = 'date_time';
-    dateTimeInput.value = new Date().toLocaleString(); // current timestamp
+    dateTimeInput.value = new Date().toLocaleString();
     form.current.appendChild(dateTimeInput);
 
     emailjs.sendForm(
@@ -21,13 +21,13 @@ function ContactUs() {
       form.current,
       'gimfEfjaE6hdhlA1x'
     )
-    .then((result) => {
-      alert('Message sent successfully!');
-      form.current.reset();
-    }, (error) => {
-      alert('Failed to send the message. Try again later.');
-      console.error(error);
-    });
+      .then((result) => {
+        alert('Message sent successfully!');
+        form.current.reset();
+      }, (error) => {
+        alert('Failed to send the message. Try again later.');
+        console.error(error);
+      });
   };
 
   return (
@@ -43,6 +43,20 @@ function ContactUs() {
           <FaFacebook className="social-icon" />
           <FaInstagram className="social-icon" />
           <FaTwitter className="social-icon" />
+        </div>
+
+        {/* Google Map Embed */}
+        <div className="map-container">
+          <iframe
+            title="Google Map"
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3804.6239482609344!2d78.3935950751681!3d17.525455783385404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTfCsDMxJzMxLjYiTiA3OMKwMjMnNDYuMiJF!5e0!3m2!1sen!2sin!4v1747533689591!5m2!1sen!2sin" 
+            width="100%"
+            height="200"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
 
